2adc24d0d99ef98cb846c713cfa31ed3b8b8585e
0JustKris0 <125268052+0JustKris0@users.noreply.github.com>
2023-04-23 18:25:22 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,52 +1,50 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-#include<string>
+#include <string>
 template<typename T, int size>
 class TPQueue {
  private:
-        T* arr;
-        int first, last, count;
+    T* arr;
+    int first, last, count;
  public:
-        TPQueue() :first(0), last(0), count(0) {
-            arr = new T[size];
+    TPQueue() : first(0), last(0), count(0), arr(new T[size]) {}
+    ~TPQueue() {
+      delete[] arr;
+    }
+    bool isEmpty() const {
+      return 0 == count;
+    }
+    bool isFull() const {
+      return size == count;
+    }
+    void push(const T& value) {
+      if (isFull()) {
+        throw std::string("Full");
+      } else {
+        int i = last;
+          for ( ; i != first; --i) {
+            if (arr[i - 1].prior < value.prior) {
+            arr[i] = arr[i - 1];
+            if (arr[(i - 1) % size].prior < value.prior) {
+            arr[i % size] = arr[(i - 1) % size];
+          } else {
+            break;
+          }
         }
-        ~TPQueue() {
-            delete[]arr;
-        }
-        bool isEmpty()const {
-            return 0 == count;
-        }
-        bool isFull()const {
-            return 0 == size;
-        }
-        void push(const T& value) {
-        if (isFull()) {
-            throw std::string("Full!");
-            } else {
-            int n = last;
-            arr[last++ % size] = value;
-            arr[last % size] = value;
-            T temp = arr[n % size];
-            while (n > first && \
-            arr[(n - 1) % size].prior < arr[n % size].prior) {
-                 temp = arr[n % size];
-                 arr[n % size] = arr[(n - 1) % size];
-                 arr[(n - 1) % size] = temp;
-                 n--;
-            }
-            ++count;
-            ++last;
-            }
-       }
-        const T& pop() {
-            if (isEmpty()) {
-                throw std::string("Empty!");
-            } else {
-                --count;
-            return arr[first++ % size];
-            }
-        }
+        arr[i % size] = value;
+        count++;
+        last++;
+      }
+    }
+    const T& pop() {
+      if (isEmpty()) {
+        throw std::string("Empty");
+      } else {
+        count--;
+        return arr[first++ % size];
+      }
+    }
 };
 struct SYM {
   char ch;
